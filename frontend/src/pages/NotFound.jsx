8fe1aa1336e5d31c35eee0266b74b374692a1620
Chaarlay <findcharliemiddleton@gmail.com>
2024-07-22 21:50:54 -0400

    first commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+function NotFound(){
+    return (
+        <div>
+            <h1>404 Not Found</h1>
+            <h2>This page does not exist!</h2>
+        </div>
+    );
+}
+export default NotFound;    
